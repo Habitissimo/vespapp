fc5937db6671e5b2358b71b91e0d12905d39667d
mariano <marianoxvii@gmail.com>
2016-03-11 18:12:24 +0100

    Integración del album

--- app/src/main/java/com/habitissimo/vespapp/Constants.java
+++ app/src/main/java/com/habitissimo/vespapp/Constants.java
@@ -4,6 +4,7 @@ import android.text.TextUtils;
 
 public class Constants {
     public static final String API_BASE_URL = "";
+    public static final String FOTOS_LIST = "com.habitissimo.vespapp.FOTOS_LIST";
 
     public static boolean isBaseApiUrlDefined() {
         return !TextUtils.isEmpty(API_BASE_URL);
