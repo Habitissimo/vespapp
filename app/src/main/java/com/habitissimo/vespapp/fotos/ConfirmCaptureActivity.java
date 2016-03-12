dd9e21bcbbedf2cfa85ef46c6d3d96cb40b98382
mariano <marianoxvii@gmail.com>
2016-03-12 11:44:42 +0100

    Implementada locaclización por gps

--- app/src/main/java/com/habitissimo/vespapp/fotos/ConfirmCaptureActivity.java
+++ app/src/main/java/com/habitissimo/vespapp/fotos/ConfirmCaptureActivity.java
@@ -1,13 +1,18 @@
 package com.habitissimo.vespapp.fotos;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -26,10 +31,13 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class ConfirmCaptureActivity extends AppCompatActivity {
+public class ConfirmCaptureActivity extends AppCompatActivity implements LocationListener {
 
     public static final String TAG = "ConfirmCaptureActivity";
     private AlertDialog dialog;
+
+    private LocationManager locationManager;
+    private Location location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,16 +48,27 @@ public class ConfirmCaptureActivity extends AppCompatActivity {
         initToolbar();
         initAlbum();
         initAddBtn();
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, this);
     }
 
     private void initToolbar() {
-        ImageView backBtn = (ImageView) findViewById(R.id.btn_toolbar_back);
-        backBtn.setOnClickListener(new View.OnClickListener() {
+        // Set a toolbar to replace the action bar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_confirm_cap);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.colorTitulo));
+        toolbar.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle(R.string.confirm_cap_titulo);
     }
 
     private void initAddBtn() {
@@ -74,8 +93,8 @@ public class ConfirmCaptureActivity extends AppCompatActivity {
         showLoading();
         Sighting sighting = new Sighting();
         sighting.type = type;
-        sighting.lat = 1;
-        sighting.lng = 2;
+        sighting.lat = location == null ? 1 : (float) location.getLatitude();
+        sighting.lng = location == null ? 2 : (float) location.getLongitude();
         Call<Sighting> call = Vespapp.get(this).getApi().createSighting(sighting);
         call.enqueue(new Callback<Sighting>() {
             @Override public void onResponse(Call<Sighting> call, Response<Sighting> response) {
@@ -155,5 +174,28 @@ public class ConfirmCaptureActivity extends AppCompatActivity {
 
     private ListaFotos getListaFotos() {
         return Database.get(this).load(Constants.FOTOS_LIST, ListaFotos.class);
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        this.location = location;
+        Toast.makeText(ConfirmCaptureActivity.this, "Latitude:" + location.getLatitude()
+                + ", Longitude:" + location.getLongitude(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
